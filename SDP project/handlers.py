8aec65e22d86b7c0eaa8f32aa69e051434d17940
Anuar <160748132+AnuarSv@users.noreply.github.com>
2024-10-27 21:50:45 +0500

    Add files via upload

---
+++ SDP project/handlers.py
@@ -0,0 +1,75 @@
+from aiogram import F, Router
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+from Gemini import gpt
+from texts import help, welcome
+import requestsDB as rq
+import keyboard as kb
+# from aiogram.types import CallbackQuery
+
+
+router = Router()
+
+
+@router.message(CommandStart())
+async def cmd_start(message: Message):
+    await rq.set_user(message.from_user.id)
+    await message.answer(welcome, reply_markup=kb.reply_kb)
+
+
+@router.message(Command('help'))
+async def cmd_help(message: Message):
+    await message.answer(help, reply_markup=kb.reply_kb)
+
+
+@router.message(Command('history'))
+async def cmd_get_history(message: Message):
+    history = await rq.get_history(message.from_user.id)
+    await message.reply(history, reply_markup=kb.reply_kb)
+
+
+@router.message(Command('del_history'))
+async def cmd_del_history(message: Message):
+    del_history = await rq.delete_history(message.from_user.id)
+    await message.reply(del_history, reply_markup=kb.reply_kb)
+
+
+# @router.callback_query(F.data == 'help')
+# async def call_help(callback: CallbackQuery):
+#     await callback.message.reply(help, reply_markup=kb.reply_kb)
+#
+#
+# @router.callback_query(F.data == 'history')
+# async def call_history(callback: CallbackQuery):
+#     history = await rq.get_history(callback.message.from_user.id)
+#     await callback.message.reply(history, reply_markup=kb.reply_kb)
+#
+#
+# @router.callback_query(F.data == 'del_history')
+# async def call_del_history(callback: CallbackQuery):
+#     del_history = await rq.delete_history(callback.message.from_user.id)
+#     await callback.message.reply(del_history, reply_markup=kb.reply_kb)
+
+@router.message(F.text == 'Help❔️')
+async def cmd_answer(message: Message):
+    await message.answer(help, reply_markup=kb.reply_kb)
+
+
+@router.message(F.text == 'My History🗒️')
+async def cmd_answer(message: Message):
+    history = await rq.get_history(message.from_user.id)
+    await message.reply(history, reply_markup=kb.reply_kb)
+
+
+@router.message(F.text == 'Delete my history 🗑️')
+async def cmd_answer(message: Message):
+    del_history = await rq.delete_history(message.from_user.id)
+    await message.reply(del_history, reply_markup=kb.reply_kb)
+
+
+@router.message(F.text)
+async def cmd_answer(message: Message):
+    history = await rq.get_history(message.from_user.id)
+    response = await gpt(message.text, history)
+    await message.reply(response)
+    await rq.save_history(message.from_user.id, f'{message.text}\n{response}')
---
+++ SDP project/texts.py
@@ -0,0 +1,47 @@
+help = '''🤖 Astana IT University Chatbot Help
+How to Use the Bot:
+
+Start: Type "Hello!" to begin the conversation.
+
+Ask Questions: Inquire about:
+
+Admission requirements
+Scholarship applications
+Course offerings
+Campus facilities
+Get Answers: The bot provides clear and specific responses. If unclear, it may ask for more details.
+
+Follow Up: Feel free to ask follow-up questions for more information.
+
+Be Specific: Provide detailed inquiries for the best assistance.
+
+Contact for Complex Issues: For detailed questions, reach out to the university's admissions office directly.
+
+Thank you for using the Astana IT University Chatbot! We're here to help you!'''
+welcome = '''Hello! I am Astana IT University Bot'''
+instruction = '''🤖 Astana IT University Chatbot Instructions
+Welcome to the Astana IT University Chatbot! This chatbot is your friendly assistant for all inquiries related to admissions and university life. Here’s how to make the most of your interaction:
+
+Start the Conversation:
+
+Simply type "Hello!" to initiate a chat. The bot will greet you warmly and be ready to assist!
+Ask Your Questions:
+
+Feel free to inquire about:
+Admission requirements
+Scholarship opportunities
+Academic programs
+Campus facilities and student life
+Receive Clear Answers:
+
+The bot provides concise and specific responses. If your question is unclear, it may ask for more details to better assist you.
+Follow Up:
+
+Don't hesitate to ask follow-up questions for additional information or clarification on any topic.
+Be Specific:
+
+The more specific your question, the better the bot can assist you. Detailed inquiries lead to more accurate responses.
+Contact for Complex Issues:
+
+For detailed or specific inquiries, consider reaching out directly to the university's admissions office.
+Thank you for using the Astana IT University Chatbot! We look forward to supporting you on your educational journey!'''
